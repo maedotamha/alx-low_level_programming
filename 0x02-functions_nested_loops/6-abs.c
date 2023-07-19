85a6f3eba88752cc7a10bbb6308ff2098b547502
maedotamha <maedotamha@gmail.com>
2023-07-19 10:55:53 -0700

    absoulute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - calculate the absoulute value.
+ *@i: input numbers
+ *Return: Absoulute value.
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
