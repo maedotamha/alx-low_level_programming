0e05b00543c2ecdca90bb5008a4853bac421a944
maedotamha <maedotamha@gmail.com>
2023-07-19 07:00:45 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+  *print_alphabet - print all alphabet
+  */
+
+void print_alphabet(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		_putchar(alpha);
+	_putchar('\n');
+}
