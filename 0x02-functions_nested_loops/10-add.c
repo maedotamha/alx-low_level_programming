8d006867316da4feedc4e6b521dca6a9661934db
maedotamha <maedotamha@gmail.com>
2023-07-19 12:26:17 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  * add - adds the two numbers
+  *@a: the number to be added
+  *@b: the number to added
+  *Return: the sum
+  */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  *print_to_98 - prints all natural numbers to 98
+  *@n: print from this number
+  */
+void print_to_98(int n
