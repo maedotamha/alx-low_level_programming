29fb1a8c3f3a67fe1ac4b7473b0d14719d029d33
maedotamha <maedotamha@gmail.com>
2023-07-20 09:42:51 -0700

    upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,7 +7,7 @@
 int _isupper(int c)
 {
 	if (c >= 97 && c <= 122)
-		return (1);
-	else
 		return (0);
+	else
+		return (1);
 }
---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+  *_isdigit - checks if the given data is a digit
+  *Return: 0 if the data is not a digit and 1 if it is
+  *@c: the data to be tested
+  */
+int _isdigit(int c)
+{
+
