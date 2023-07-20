0615575909e4e18298ce363416849178e30fece6
maedotamha <maedotamha@gmail.com>
2023-07-20 13:40:45 -0700

    ./10-triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -15,13 +15,17 @@ void print_triangle(int size)
 
 		for (i = 1; i <= size; i++)
 		{
-			for (j = 1; j < size; j++)
+			for (j = size - i; j > 0; j--)
 			{
 				_putchar(' ');
 			}
-			for (j = 1; j <= i; j++)
+			for (j = 0; j < i; j++)
 			{
 				_putchar('#');
+			}
+			if (i == size)
+			{
+				continue;
 			}
 			_putchar('\n');
 		}
