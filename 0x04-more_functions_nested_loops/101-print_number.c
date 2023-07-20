db4d6a3d696cc48dbb2e8f26f650c16e1c221f35
maedotamha <maedotamha@gmail.com>
2023-07-20 14:14:09 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 101-main.c 101-print_number.c -o 101-print_numbers

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  *print_number - prints an int
+  *@n: the integer that is put in
+  */
+void print_number(int n)
+{
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+	if ((num / 10) > 0)
+		print_number(num / 10);
+	_putchar((num % 10) + '0');
+}
