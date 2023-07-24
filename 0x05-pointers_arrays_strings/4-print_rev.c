3af11030d9f2c874a346bf2e4c73ff6eb4b4da0d
maedotamha <maedotamha@gmail.com>
2023-07-24 11:48:55 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+  * print_rev - print the rivers
+  *@s: the string input
+  *Return: 0 always
+  */
+void print_rev(char *s)
+{
+	int i, leng = 0;
+
+	while (*s != '\0')
+	{
+		leng++;
+		i++;
+	}
+	for (i = leng; i > 0; i--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n\);
+}
