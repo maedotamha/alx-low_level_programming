5c4eccf006f207c4054c762f43755409834ebbd4
maedotamha <maedotamha@gmail.com>
2023-07-24 12:06:50 -0700

    putchar

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,10 @@
+#include <unistd>
+/**
+  * _putchar - writes the charachter
+  *@c: the character to be entered
+  *Return: on success 1
+  */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
