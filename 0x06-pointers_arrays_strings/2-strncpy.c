9119830a99c05168d4b0070057ed9337f497f062
maedotamha <maedotamha@gmail.com>
2023-07-27 10:48:36 -0700

    two

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,1 +1,25 @@
-#include
+#include "main.h"
+/**
+  *_strncpy - Copies one string onto another
+  *@dest: Input value
+  *@src: Input value
+  *@n: The integer input
+  *Return: dest
+  */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j = 0;
+
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+
+	return (dest);
+}
