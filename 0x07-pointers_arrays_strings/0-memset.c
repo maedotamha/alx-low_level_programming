39e38cf1383145ead34f9f3607bf0244fa55fec5
maedotamha <maedotamha@gmail.com>
2023-08-01 03:32:24 -0700

    all

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,12 +10,12 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-        int a = 0;
+	int a = 0;
 
-        for (; n > 0; a++)
-        {
-                s[a] = b;
-                n--;
-        }
-        return (s);
+	for (; n > 0; a++)
+	{
+		s[a] = b;
+		n--;
+	}
+	return (s);
 }
--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,13 +10,13 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-        int a = 0;
-        int b = n;
+	int a = 0;
+	int b = n;
 
-        for (; a < b; a++)
-        {
-                dest[a] = src[a];
-                n--;
-        }
-        return (dest);
+	for (; a < b; a++)
+	{
+		dest[a] = src[a];
+		n--;
+	}
+	return (dest);
 }
