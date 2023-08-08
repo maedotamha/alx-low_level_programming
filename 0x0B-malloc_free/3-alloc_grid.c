15489a8b32ac5cdae866357103196af773d4a3eb
maedotamha <maedotamha@gmail.com>
2023-08-08 13:38:17 -0700

    fourth

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,7 +14,6 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 	a = malloc(sizeof(int *) * height);
-	
 	if (a == NULL)
 		return (NULL);
 	for (x = 0; x < height; x++)
