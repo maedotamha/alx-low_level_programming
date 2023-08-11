2a5f6fca769fe92ca945171b72beabf41a34e553
maedotamha <maedotamha@gmail.com>
2023-08-10 20:05:36 -0700

    all

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,31 +10,28 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-        char *p1;
-        char *o_p;
-        unsigned int i;
+	char *p1;
+	char *o_p;
+	unsigned int i;
 
-        if (new_size == old_size)
-                return (ptr);
-
-        if (new_size == 0 && ptr)
-        {
-                free(ptr);
-                return (NULL);
-        }
-        if (!ptr)
-                return (malloc(new_size));
-        p1 = malloc(new_size);
-        if (!p1)
-                return (NULL);
-
-        o_p = ptr;
-
-        if (new_size > old_size)
-        {
-                for (i = 0; i < old_size; i++)
-                        p1[i] = o_p[i];
-		}
-        free(ptr);
-        return (p1);
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (!ptr)
+		return (malloc(new_size));
+	p1 = malloc(new_size);
+	if (!p1)
+		return (NULL);
+	o_p = ptr;
+	if (new_size > old_size)
+	{
+		for (i = 0; i < old_size; i++)
+			p1[i] = o_p[i];
+	}
+	free(ptr);
+	return (p1);
 }
